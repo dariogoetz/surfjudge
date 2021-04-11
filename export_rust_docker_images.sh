e467f7b928f27df53fcd9a5244b10c93b5cc4404
Dario Götz <dario.goetz@googlemail.com>
2021-04-11 22:34:55 +0200

    [mod] update im/export scripts

--- export_rust_docker_images.sh
+++ export_rust_docker_images.sh
@@ -1,4 +1,4 @@
-for img in surfjudge-rust-websocket surfjudge-public
+for img in surfjudge-rust-websocket surfjudge-actix
 do
     echo "Exporting docker image $img to $img.tar.gz"
     sudo docker save $img | gzip > "$img.tar.gz"
--- import_rust_docker_images.sh
+++ import_rust_docker_images.sh
@@ -1,4 +1,4 @@
-for img in surfjudge-rust-websocket surfjudge-public
+for img in surfjudge-rust-websocket surfjudge-actix
 do
     zcat "$img.tar.gz" | sudo docker load
 done
