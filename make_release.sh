b25e0196e28c6bdcebfac59ac3608d78d7611044
Dario Götz <dario.goetz@googlemail.com>
2022-04-03 21:57:46 +0200

    [mod] remove reference to non-existing install script

--- make_release.sh
+++ make_release.sh
@@ -5,4 +5,4 @@ echo "Generating frontend"
 (cd surfjudge-vue-frontend && npm run prod && npm run prod-judging)
 
 echo "Compiling archive"
-tar cfvz release-$(date +%Y.%m.%d_%H.%M).tar.gz docker-compose.yml surfjudge-*.tar.gz surfjudge-vue-frontend/dist import_docker_images.sh install_release.sh surfjudge-actix/nginx/nginx.conf surfjudge-actix/nginx/nginx-judging.conf surfjudge-pyramid/nginx/nginx_traefik.conf .env surfjudge-pyramid/app
+tar cfvz release-$(date +%Y.%m.%d_%H.%M).tar.gz docker-compose.yml surfjudge-*.tar.gz surfjudge-vue-frontend/dist import_docker_images.sh surfjudge-actix/nginx/nginx.conf surfjudge-actix/nginx/nginx-judging.conf surfjudge-pyramid/nginx/nginx_traefik.conf .env surfjudge-pyramid/app
